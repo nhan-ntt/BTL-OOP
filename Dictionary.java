fb622321975208d45cfc5c2cf1fcc75a30aeaded
nhanteefu <22028114@vnu.edu.vn>
2023-10-06 13:04:50 +0700

    declare Word and Dictionary

---
+++ Dictionary.java
@@ -0,0 +1,11 @@
+public class Dictionary extends Word{
+    private ArrayList<Word> words = new ArrayList<Word>();
+    public Dictionary() {}
+    public Dictionary(String word_target, String word_explain) {}
+    public ArrayList<Word> getWords() {}
+    public void setWords(ArrayList<Word> words) {}
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Word.java
@@ -0,0 +1,43 @@
+public class Word {
+    private String word_target;
+    private String word_explain;
+
+    /**
+     * Construct.
+     * @param word_target english.
+     * @param word_explain vietnamese.
+     */
+    public Word() {
+        this.word_target = "";
+        this.word_explain = "";
+    }
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+    public String getWord_target() {
+        return word_target;
+    }
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+    public String getWord_explain() {
+        return word_explain;
+    }
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+    @Override
+    public String toString() {
+        return "Word{" + "word_target=" + word_target + ", word_explain=" + word_explain + '}';
+    }
+
+    public static void main(String[] args) {
+        Word word = new Word();
+        word.setWord_target("hello");
+        word.setWord_explain("hello");
+        System.out.println(word);
+        System.out.println(word.toString());
+        System.out.println(word.getWord_target());
+    }
+}
